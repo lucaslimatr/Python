48e5868fa5e14030542f6461cb20f58d7ff11c77
Lucas Lima <lucaslimatr@gmail.com>
2022-12-10 11:59:26 -0300

    Delete ex057.py

--- cursoemvideo/Mundo_02/ex057.py
+++
@@ -1,8 +0,0 @@
-sexo = input('Informe seu sexo [M/F]: ').strip().upper()[0]
-while sexo not in 'MF':
-    sexo = input('Dados inválidos! Informe seu sexo [M/F]: ').strip().upper()[0]
-if sexo == 'M':
-    sexo = 'MASCULINO'
-if sexo == 'F':
-    sexo = 'FEMININO'
-print('Sexo {} registrado com sucesso'.format(sexo))
