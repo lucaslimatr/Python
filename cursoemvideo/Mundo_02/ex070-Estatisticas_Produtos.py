db7a6c530673fe3770848b815974a6da4da33ceb
Lucas Lima <lucaslimatr@gmail.com>
2023-01-30 14:20:45 -0300

    att

--- cursoemvideo/Mundo_02/ex070-Estatisticas_Produtos.py
+++ cursoemvideo/Mundo_02/ex070-Estatisticas_Produtos.py
@@ -1,5 +1,5 @@
 from time import sleep
-continuar = preco = prod = total = cont = contpreco = menor_valor = mais_barato = 0
+total = cont = contpreco = menor_valor = mais_barato = 0
 print('~' * 20,'\nLISTA DE PRODUTOS')
 print('~' * 20)
 while True:
@@ -7,21 +7,18 @@ while True:
     preco = float(input('Digite o valor do produto: '))
     continuar = input('Deseja continuar [S/N]? ').upper().strip()[0]
     cont += 1
-    while continuar not in ['S', 'N']:
+    while continuar not in 'SN':
         print('Opção inválida')
         sleep(1)
-        continuar = input('Deseja continuar [S/N]? ')
+        continuar = input('Deseja continuar [S/N]? ').upper().strip()[0]
     total += preco
     if preco > 1000:
         contpreco += 1
-    if cont == 1:
-        menor_valor = preco
-        mais_barato = prod
-    if preco < menor_valor:
+    if cont == 1 or preco < menor_valor:
         menor_valor = preco
         mais_barato = prod
     if continuar == 'N':
         break
 print('~' * 20)
-print(f'Total gasto na compra: R${total}\n{contpreco}produtos custam mais de R$1000.00\nO mais barato foi: {mais_barato}')
-        
+print(f'Total gasto na compra: R${total:.2f}\n{contpreco} produtos custam mais de R$1000.00')
+print(f'O mais barato foi: {mais_barato} - R${menor_valor:.2f}')
