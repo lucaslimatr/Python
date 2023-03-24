ace3ca032389e9f9f8d169d087ddbc99cff184c5
Lucas Lima <lucaslimatr@gmail.com>
2023-03-24 16:39:58 -0300

    att

--- udemy/aulas/aula053.py
+++ udemy/aulas/aula053.py
@@ -4,23 +4,18 @@ None = Não valor
 is e is not = é ou não é (tipo, valor, identidade)
 id = Identidade
 """
-# condicao = False
-# passou_no_if = None
+condicao = False
+passou_no_if = None
 
-# if condicao:
-#     passou_no_if = True
-#     print('Faça algo')
-# else:
-#     print('Não faça algo')
+if condicao:
+    passou_no_if = True
+    print('Faça algo')
+else:
+    print('Não faça algo')
 
 
-# if passou_no_if is None:
-#     print('Não passou no if')
-# else:
-#     print('Passou no if')
+if passou_no_if is None:
+    print('Não passou no if')
+else:
+    print('Passou no if')
 
-nome = 'lucas'
-
-print(nome)
-
-len(nome)
