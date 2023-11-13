a950f680dd27d5743e26d370a34f7128b568dc96
Lucas Lima <lucaslimatr@gmail.com>
2023-11-13 16:35:29 -0300

    att

--- udemy/aulas/aula018_print.py
+++ udemy/aulas/aula018_print.py
@@ -5,3 +5,9 @@ print('Lucas', 'dos', 'Santos', 'Lima', sep='-')
 
 print('824', '176', '070', sep='.', end='-')
 print('18')
+
+# \r\n -> CRLF = CR/LF (carriage return AND line feed) - Quebra de linha utilizado no Windows
+# \n -> LF - Quebra de linha utilizado no Linux e Mac
+print(12, 34, 1011, sep="", end='#')
+print(56, 78, sep='-', end='\n')
+print(9, 10, sep='-', end='\n')
