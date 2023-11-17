11ddb75b0e1685be702127c0485cb024a0e6e96f
Lucas Lima <lucaslimatr@gmail.com>
2023-11-17 08:08:30 -0300

    Update aula047_exercicio.py

--- udemy/aulas/aula047_exercicio.py
+++ udemy/aulas/aula047_exercicio.py
@@ -19,9 +19,12 @@ idade = input('Digite sua idade: ')
 if nome and idade:
     print(f'Seu nome é {nome}')
     print(f'Seu nome invertido é {nome[::-1]}')
-    print(f'Seu nome contém {len(nome)} espaços')
+    if ' ' in nome:
+        print('Seu nome contém espaços')
+    else:
+        print('Seu nome não contem espaços')
     print(f'A primeira letra do seu nome é {nome[0]}')
     print(f'A última letra do seu nome é {nome[-1]}')
 else:
-    print(f'Desculpe, você deixou campos vazias.')
+    print(f'Desculpe, você deixou campos vazios.')
     
