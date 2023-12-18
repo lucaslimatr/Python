c0d6df7ddc111d7be0f6a440ceaf608d71c83e29
Lucas Lima <lucaslimatr@gmail.com>
2023-12-18 09:46:02 -0300

    Update aula064_exercicio_while.py

--- udemy/aulas/aula064_exercicio_while.py
+++ udemy/aulas/aula064_exercicio_while.py
@@ -4,10 +4,11 @@ Iterando strings com while
 #       012345678910
 nome = 'Luiz Otávio'  # Iteráveis
 #      1110987654321
-tamanho_nome = len(nome)
-print(nome)
-print(tamanho_nome)
-print(nome[3])
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += letra
+    indice += 1
 
-nova_string = ''
-nova_string += '*L*u*i*z* *O*t*á*v*i*o'
+print(novo_nome)
