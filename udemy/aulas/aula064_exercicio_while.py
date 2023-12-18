bed135ab3cc0ff4dbb2ce88195346273edd903f4
Lucas Lima <lucaslimatr@gmail.com>
2023-12-18 09:49:56 -0300

    Update aula064_exercicio_while.py

--- udemy/aulas/aula064_exercicio_while.py
+++ udemy/aulas/aula064_exercicio_while.py
@@ -8,7 +8,7 @@ indice = 0
 novo_nome = ''
 while indice < len(nome):
     letra = nome[indice]
-    novo_nome += letra
+    novo_nome += f'*{letra}'
     indice += 1
-
+novo_nome += '*'
 print(novo_nome)
