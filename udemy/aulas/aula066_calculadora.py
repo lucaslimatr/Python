7588f4233a4980f81f3bc6c148bff6d849afab3e
Lucas Lima <lucaslimatr@gmail.com>
2023-12-28 17:14:17 -0300

    Create aula066_calculadora.py

---
+++ udemy/aulas/aula066_calculadora.py
@@ -0,0 +1,37 @@
+primeiro_numero = 0
+segundo_numero = 0
+operador = 0
+resultado = 0
+
+while True:
+    try:
+        primeiro_numero = input('Digite um número: ')
+        primeiro_numero = int(primeiro_numero)
+        
+        operador = input('Digite um operador: ')
+
+        segundo_numero = input('Digite um número: ')
+        segundo_numero = int(segundo_numero)
+
+        if operador == '+':
+            resultado = primeiro_numero + segundo_numero
+            print(resultado)
+        elif operador == '-':
+            resultado = primeiro_numero - segundo_numero
+            print(resultado)
+        elif operador == '*':
+            resultado = primeiro_numero * segundo_numero
+            print(resultado)
+        elif operador == '/':
+            resultado = primeiro_numero / segundo_numero
+            print(resultado)
+        else:
+            print('Operação inválida!')
+    except:
+        print('Operação inválida!')
+    
+    continuar = input('Deseja continuar? [S/N] = ')
+    continuar = continuar.upper()
+    if continuar == 'N':
+        break
+print('Fim')
